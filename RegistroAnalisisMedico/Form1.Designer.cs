c83efc141dc9a2c6646f07fd3fb6ab3667603979
Rosy <Rosy@DESKTOP-HI3GI4E>
2019-06-11 19:45:58 -0400

    Comenzando el proyecto en el cual estoy registrando los analisis de un medico

--- RegistroAnalisisMedico/Form1.Designer.cs
+++ RegistroAnalisisMedico/Form1.Designer.cs
@@ -75,7 +75,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Analisis Medico";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
